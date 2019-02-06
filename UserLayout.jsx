f30f9ae8b5b0f8a7696859dff99697674520716d
iampreethamveigas <veigasbros@gmail.com>
2019-02-06 22:42:35 +0530

    title removed

--- UserLayout.jsx
+++ UserLayout.jsx
@@ -47,10 +47,8 @@ const UserLayout = props => {
               <div className={styles.header}>
                 <Link to="/">
                   <img alt="logo" className={styles.logo} src={logo} />
-                  {/* <span className={styles.title}>Afya </span> */}
                 </Link>
               </div>
-              {/* <div className={styles.desc}>Afya tag-line</div> */}
             </div>
 
             <div className={styles.sidebar_content}>

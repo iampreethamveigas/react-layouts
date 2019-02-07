0245ce52c5006c33419f4470b51ca577731179a8
iampreethamveigas <veigasbros@gmail.com>
2019-02-07 22:43:50 +0530

    library tree

--- UserLayout.jsx
+++ UserLayout.jsx
@@ -1,7 +1,7 @@
+import React from 'react';
 import { DefaultFooter, getMenuData, getPageTitle } from '@ant-design/pro-layout';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Link, useIntl, connect } from 'umi';
-import React, { useState, useEffect } from 'react';
 import logo from '../assets/whilelogo.svg';
 import styles from './UserLayout.less';
 
